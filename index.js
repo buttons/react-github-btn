4e51e2534a64d1cbada786954fa75e22e384fa26
なつき <i@ntk.me>
2019-02-18 05:24:08 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+import { render } from 'github-buttons'
+
+class GitHubButton extends Component {
+  constructor (props) {
+    super(props)
+    this.ref = props.ref || React.createRef()
+  }
+  render () {
+    return React.createElement('a', { ...this.props, ref: this.ref }, this.props.children)
+  }
+  componentDidMount () {
+    this.paint()
+  }
+  componentWillUpdate () {
+    this.reset()
+  }
+  componentDidUpdate () {
+    this.paint()
+  }
+  componentWillUnmount () {
+    this.reset()
+  }
+  paint () {
+    (function ($el, _) {
+      render($el.parentNode.insertBefore(_, $el).appendChild($el), function (el) {
+        try {
+          _.replaceChild(el, $el)
+        } catch (_) {}
+      })
+    })(this.ref.current, this._ = document.createElement('span'))
+  }
+  reset () {
+    this._.parentNode.replaceChild(this.ref.current, this._)
+  }
+}
+
+export default GitHubButton
